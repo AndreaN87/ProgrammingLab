86a973e281ce2b4919f2d009beb706b452e7ccaa
repl.it user <replituser@example.com>
2020-12-23 10:14:05 +0000

    Svolto esercizio 1 + modificato file intro

---
+++ es1_somma_lista.py
@@ -0,0 +1,10 @@
+#Scrivete una funzione che sommi tutti gli elementi di una lista
+
+#Definisco una funzione
+def somma_lista(lista):
+    return sum(lista)
+
+#Creo una lista
+unalista = [1,4,10,10]
+#Stampo la somma della lista
+print(f"La somma della lista equivale a :{somma_lista(unalista)}")
